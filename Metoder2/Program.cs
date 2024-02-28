1be85186f41964a6f99475554b7f36e4c330285b
baronvonpurr <robinhood.svensson@hotmail.com>
2024-02-28 10:07:57 +0100

    Add project files.

---
+++ Metoder2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Metoder2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[2];
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Skriv in ett tal");
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(störst(tal[0], tal[1]));
+        }
+
+        static int störst(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+            else
+            {
+                return tal2;
+            }
+        }
+    }
+}
